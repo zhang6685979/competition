f67c961784be957555a59ed2dadb135bd371b8ab
zhang6685979 <shui_1226@126.com>
2022-09-22 14:03:43 +0800

    修改不能下载附件问题

--- src/pages/website/components/DownloadList.vue
+++ src/pages/website/components/DownloadList.vue
@@ -42,18 +42,21 @@
     },
     methods:{
 	   downloadFile(url) {
-	     var fileName = decodeURIComponent(url.substring(url.lastIndexOf("/")+1))
-	     this.$http.get(url)
-	     .then(res => {
-	         const blob = new Blob([res.data], {type: res.headers['content-type']});
-	         const a = document.createElement("a");
-	         const objectUrl = window.URL.createObjectURL(blob);
-	         a.download = fileName;
-	         a.href = objectUrl;
-	         a.click();
-	         window.URL.revokeObjectURL(objectUrl);
-	         a.remove();
-	     })
+	     var fileName = decodeURIComponent(url.substring(url.lastIndexOf("/") + 1))
+	     const el = document.createElement('a');
+	     el.style.display = 'none';
+	     el.setAttribute('target', '_blank');
+	     /**
+	      * download的属性是HTML5新增的属性
+	      * href属性的地址必须是非跨域的地址，如果引用的是第三方的网站或者说是前后端分离的项目(调用后台的接口)，这时download就会不起作用。
+	      * 此时，如果是下载浏览器无法解析的文件，例如.exe,.xlsx..那么浏览器会自动下载，但是如果使用浏览器可以解析的文件，比如.txt,.png,.pdf....浏览器就会采取预览模式
+	      * 所以，对于.txt,.png,.pdf等的预览功能我们就可以直接不设置download属性(前提是后端响应头的Content-Type: application/octet-stream，如果为application/pdf浏览器则会判断文件为 pdf ，自动执行预览的策略)
+	      */
+	     fileName && el.setAttribute('download', fileName);
+	     el.href = url;
+	     document.body.appendChild(el);
+	     el.click();
+	     document.body.removeChild(el);
 	   }
     }
   }
